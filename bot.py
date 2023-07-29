b0dfd09d53857515efdf46af89b5932bd54d91b9
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-07-30 02:22:46 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import creator:
 import account; 
 import time 
   
- import config from "config.json"
+from config import config
 # Creating accounts 
 
 from tiktok import accounts 
@@ -14,7 +14,7 @@ from account import email, password, username
 from proxies import * 
 
 creator.init(tiktok.com --accounts)
-def __accountCreator__:
+def __accountCreator__():
   x=creator.newAccount(tiktok, email, password, username) 
     x.append()
       x.changeAvatar(RandomPictureNet) 
